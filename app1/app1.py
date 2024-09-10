5781cd727b2ce0a966045cf5e91816a095aff1ab
euuuuuukov <05.kova.05@gmail.com>
2024-09-10 13:02:20 +0300

    lab1

---
+++ app1/app1.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get('/', response_class=HTMLResponse)
+def main() -> str:
+    return '''
+    <html>
+        <head>
+            <title>itmo_devops_clouds_labs</title>
+        </head>
+        <body>
+            <h1>Первое приложение</h1>
+        </body>
+    </html>
+    '''
